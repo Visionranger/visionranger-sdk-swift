a8e4474616c95efa7d0392f9f03d57c7676c9688
Colin Tessarzick <colin@visionranger.com>
2021-09-24 00:35:28 +0200

    -

---
+++ Visionranger/VSNProductConfiguration.swift
@@ -0,0 +1,120 @@
+//
+//  VSNProductConfiguration.swift
+//  VSNProductConfiguration
+//
+//  Created by Colin Tessarzick on 23.09.21.
+//
+
+import Foundation
+
+public class VSNProductConfiguration: NSObject {
+    
+    /// The unique identifier of the product's configuration from the Visionranger API
+    public let configID: String?
+    
+    /// The unique identifier of the product, this configuration belongs to
+    public let associatedProductID: String?
+    
+    /// The price of the product in this configuration
+    public var price: Double?
+    
+    /// The global index of the main material, associated to this product configuration
+    ///
+    /// The main material is defined as the material that covers at least 50% of the visual surface of a product.
+    public var material: Int?
+    
+    /// The color of the main material, associated to this product configuration
+    public var color: String?
+    
+    /// The URL that leads to this product's configuration
+    public var modelURL: String?
+    
+    /// The average time range in days, that this product configuration requires from ordering to delivery.
+    /// This value represents the index of `VSNProductDeliveryRange
+    public var deliveryIndex: Int?
+    
+    /// A 3 dimensional array of Doubles, defined as `[length, width, height]`
+    public var dimensions: VSNProductDimensions?
+    
+    /// The weight of the product configuration, measured in kilogramm
+    public var weight: Double?
+    
+    public var allResponseFields: [AnyHashable: Any]
+    
+    public convenience init(configID: String, associatedProductID: String, modelURL: String) {
+        self.init(
+            configID: configID,
+            associatedProductID: associatedProductID,
+            material: 0,
+            modelURL: modelURL,
+            dimensions: VSNProductDimensions(),
+            weight: 0,
+            allResponseFields: [:]
+        )
+    }
+    
+    internal init(
+        configID: String,
+        associatedProductID: String,
+        material: Int,
+        modelURL: String,
+        dimensions: VSNProductDimensions?,
+        weight: Double,
+        allResponseFields: [AnyHashable: Any]
+    ) {
+        self.configID = configID
+        self.associatedProductID = associatedProductID
+        self.material = material
+        self.modelURL = modelURL
+        self.dimensions = dimensions
+        self.weight = weight
+        self.allResponseFields = allResponseFields
+        super.init()
+    }
+    
+    convenience override init() {
+        self.init(
+            configID: "",
+            associatedProductID: "",
+            material: 0,
+            modelURL: "",
+            dimensions: VSNProductDimensions(),
+            weight: 0,
+            allResponseFields: [:]
+        )
+    }
+}
+
+extension VSNProductConfiguration: VSNAPIResponseDecodable {
+    
+    public class func decodeObject(fromAPIResponse response: [AnyHashable : Any]?) -> Self? {
+        guard let dict = response,
+        let configID = dict["id"] as? String,
+        let productID = dict["product_id"] as? String
+        else {
+            return nil
+        }
+        
+        let productDimensions: VSNProductDimensions?
+        
+        if let dimensionsDict = dict["dimensions"] as? [AnyHashable: Any],
+           let dimensions = VSNProductDimensions.decodeObject(fromAPIResponse: dimensionsDict) {
+            dimensions.length = dimensionsDict["length"] as? Double
+            dimensions.width = dimensionsDict["width"] as? Double
+            dimensions.height = dimensionsDict["height"] as? Double
+            productDimensions = dimensions
+        } else {
+            productDimensions = nil
+        }
+        
+        return VSNProductConfiguration(
+            configID: configID,
+            associatedProductID: productID,
+            material: dict["material"] as! Int,
+            modelURL: dict["model_url"] as! String,
+            dimensions: productDimensions,
+            weight: dict["weight"] as! Double,
+            allResponseFields: dict
+        ) as? Self
+    }
+}
---
+++ Visionranger/VSNProductDimensions.swift
@@ -0,0 +1,72 @@
+//
+//  VSNProductDimensions.swift
+//  VSNProductDimensions
+//
+//  Created by Colin Tessarzick on 23.09.21.
+//
+
+import Foundation
+
+/// Product dimensions and volume are important values for determining the product size category.
+///
+/// In addition to product weight, dimensions are used to calculate a variety of fees for shipping through Visionranger's verified panrter businesses, including storage fees, shipping fees, assembly fees and handling fees for merchandise returns.
+public class VSNProductDimensions: NSObject {
+    
+    /// The length of the product in centimeter
+    public var length: Double?
+    
+    /// The height of the product in centimeter
+    public var height: Double?
+    
+    /// The width of the product in centimeter
+    public var width: Double?
+    
+    public var allResponseFields: [AnyHashable : Any]
+    
+    /// A convenient way of initialising a `VSNProductDimensions` object
+    /// - Parameter dimensions: An array of type `Double`. The order of values is important!
+    ///
+    /// To initialize the object correctly, write the array as follows:
+    /// ```
+    /// [length, height, width]
+    /// ```
+    public convenience init(_ dimensions: [Double]) {
+        let length = Double(dimensions.index(0, offsetBy: 0))
+        let height = Double(dimensions.index(1, offsetBy: 0))
+        let width = Double(dimensions.index(2, offsetBy: 0))
+        
+        self.init(length: length, height: height, width: width, allResponseFields: [:])
+    }
+    
+    internal init(length: Double, height: Double, width: Double, allResponseFields: [AnyHashable: Any]) {
+        self.length = length
+        self.height = height
+        self.width = width
+        self.allResponseFields = allResponseFields
+        super.init()
+    }
+    
+    /// Initializes a new `VSNProductDimensions` object with a length, height and width of 0 centimeters.
+    convenience override init() {
+        self.init(length: 0, height: 0, width: 0, allResponseFields: [:])
+    }
+}
+
+extension VSNProductDimensions: VSNAPIResponseDecodable {
+    
+    
+    public class func decodeObject(fromAPIResponse response: [AnyHashable : Any]?) -> Self? {
+        guard let dict = response else {
+            return nil
+        }
+        
+        let dimensions = VSNProductDimensions()
+        dimensions.allResponseFields = dict
+        /// All propertes are nullable
+        dimensions.length = dict["length"] as? Double
+        dimensions.width = dict["width"] as? Double
+        dimensions.height = dict["height"] as? Double
+        
+        return dimensions as? Self
+    }
+}
