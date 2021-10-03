888a31b5c1feb27584cda1b52cf03f8bccd3d590
Colin Tessarzick <colin@visionranger.com>
2021-10-03 15:27:10 +0200

    -

--- Basic Integration/Basic Integration/AppDelegate.swift
+++ Basic Integration/Basic Integration/AppDelegate.swift
@@ -31,8 +31,6 @@ import Visionranger
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         VisionrangerAPI.defaultAccessKey = ""
