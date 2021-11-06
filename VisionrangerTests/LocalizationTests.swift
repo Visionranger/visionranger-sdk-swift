db6f48dd3ce0f72929345fd70e7852dc882304f4
Colin Tessarzick <colin@visionranger.com>
2021-11-06 15:08:02 +0100

    -

--- VisionrangerTests/LocalizationTests.swift
+++ VisionrangerTests/LocalizationTests.swift
@@ -1,8 +1,8 @@
 //
-//  VisionrangerAPIConfiguration+Version.swift
+//  LocalizationTests.swift
 //  Visionranger
 //
-//  Created by Colin Tessarzick on 27.09.21.
+//  Created by Colin Tessarzick on 06.11.21.
 //
 //  Copyright © 2020-2021 Visionranger e.K. All rights reserved.
 //
@@ -23,10 +23,30 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
+//
 
-import Foundation
+import XCTest
 
-extension VisionrangerAPIConfiguration {
-    
-    static let VSNSDKVersion = "0.4.0"
+class LocalizationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
 }
