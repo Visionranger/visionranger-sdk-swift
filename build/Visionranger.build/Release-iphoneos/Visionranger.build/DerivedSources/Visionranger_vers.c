e671a3142688ea65cadfde7d3df838acc5ba718a
Colin Tessarzick <63814520+colintessarzick@users.noreply.github.com>
2021-12-04 09:02:32 +0100

    setup: unify framework versioning (#43)

---
+++ build/Visionranger.build/Release-iphoneos/Visionranger.build/DerivedSources/Visionranger_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char VisionrangerVersionString[];
+ extern const double VisionrangerVersionNumber;
+
+ const unsigned char VisionrangerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Visionranger  PROJECT:Visionranger-1" "\n";
+ const double VisionrangerVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/Visionranger.build/Release-iphoneos/Visionranger.build/Objects-normal/arm64/Visionranger_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/colintessarzick/Desktop/SDKs/visionranger-sdk-swift/build/Visionranger.build/Release-iphoneos/Visionranger.build/DerivedSources/Visionranger_vers.c
